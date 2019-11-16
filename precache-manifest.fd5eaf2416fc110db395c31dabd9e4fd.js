7e4a1329c0ca4c2050e1dc8c4a99fa645dfa262d
Manish <spyde916@gmail.com>
2019-11-16 21:07:16 +0530

    Updates

---
+++ precache-manifest.fd5eaf2416fc110db395c31dabd9e4fd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acffe21ff9f6770a5e3b43345c87c3ad",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "40f34cf23e0fddda37ac",
+    "url": "/myapp/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "f0edf432b3f2fdf7a295",
+    "url": "/myapp/static/css/main.e9b64536.chunk.css"
+  },
+  {
+    "revision": "40f34cf23e0fddda37ac",
+    "url": "/myapp/static/js/2.edee88df.chunk.js"
+  },
+  {
+    "revision": "f0edf432b3f2fdf7a295",
+    "url": "/myapp/static/js/main.cb7090d0.chunk.js"
+  },
+  {
+    "revision": "fefa2450c5ac373150ef",
+    "url": "/myapp/static/js/runtime-main.0af31a26.js"
+  },
+  {
+    "revision": "2fa12a11d347ba5ae19a7dd873dc0987",
+    "url": "/myapp/static/media/bgnew3.2fa12a11.jpg"
+  },
+  {
+    "revision": "e4cdb8e5090076ed4e7bb4fca4d135d0",
+    "url": "/myapp/static/media/contact3.e4cdb8e5.jpg"
+  },
+  {
+    "revision": "cece0dcc532d947c00a72c300a69ef84",
+    "url": "/myapp/static/media/contact7.cece0dcc.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/myapp/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "1608d800a4953e3a48200866ea8220b6",
+    "url": "/myapp/static/media/logom.1608d800.png"
+  },
+  {
+    "revision": "ccb77b3439bd2693154fce17a1858333",
+    "url": "/myapp/static/media/manish.ccb77b34.jpg"
+  },
+  {
+    "revision": "382e7953153017ebd0ab722de024a3fd",
+    "url": "/myapp/static/media/robo.382e7953.jpg"
+  },
+  {
+    "revision": "4600bdd055be1d9e45032dba0427713a",
+    "url": "/myapp/static/media/woody.4600bdd0.jpg"
+  }
+]);
